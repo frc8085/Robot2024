1b2a66b9dfad04ff02c888399182d7656cca9eac
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2024-02-10 16:26:58 -0500

    fixed controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,190 +51,193 @@ import java.util.List;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems
-    private final DriveSubsystem m_drive = new DriveSubsystem();
-    private final ShooterSubsystem m_shooter = new ShooterSubsystem();
-    private final IntakeSubsystem m_intake = new IntakeSubsystem();
-    private final ArmSubsystem m_arm = new ArmSubsystem();
-    private final ClimberSubsystem m_climb = new ClimberSubsystem();
+        // The robot's subsystems
+        private final DriveSubsystem m_drive = new DriveSubsystem();
+        private final ShooterSubsystem m_shooter = new ShooterSubsystem();
+        private final IntakeSubsystem m_intake = new IntakeSubsystem();
+        private final ArmSubsystem m_arm = new ArmSubsystem();
+        private final ClimberSubsystem m_climb = new ClimberSubsystem();
 
-    // The driver's controller
-    CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
-    // The operator's controller
-    CommandXboxController m_operatorController = new CommandXboxController(OIConstants.kOperatorControllerPort);
-
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the button bindings
-        configureButtonBindings();
-        addToDashboard();
-
-        // Configure default commands
-        m_drive.setDefaultCommand(
-                // The left stick controls translation of the robot.
-                // Turning is controlled by the X axis of the right stick.
-                new RunCommand(() -> m_drive.drive(-MathUtil.applyDeadband(m_driverController.getLeftY(),
-                        OIConstants.kDriveDeadband),
-                        -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                        -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
-                        true,
-                        true),
-                        m_drive));
-    }
-
-    private void addToDashboard() {
-        SmartDashboard.putData("Move to Podium", new MoveToPosition(m_arm, ArmConstants.Position.PODIUM));
-
-    }
-
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     */
-    private void configureButtonBindings() {
-        // DRIVER controlled buttons
-        final Trigger shoot = m_driverController.a();
-
-        // OPERATOR controlled buttons
-        final Trigger intake = m_operatorController.leftTrigger();
-        final Trigger alternatePosition = m_operatorController.leftBumper();
-
-        final Trigger moveToTravel = m_operatorController.y();
-        final Trigger moveToSubwoofer = m_operatorController.b();
-        final Trigger moveToAmp = m_operatorController.a();
-        final Trigger moveToPodium = m_operatorController.x();
-        final Trigger moveToTrapApproach = m_operatorController.povUp();
-        final Trigger moveToTrapScore = m_operatorController.povRight();
-        final Trigger moveToTrapClimb = m_operatorController.povDown();
-
-        // manual arm and shooter movement - arm left joystick, shooter right joystick
-        final Trigger ArmRaiseButton = m_operatorController.axisLessThan(1, -.25);
-        final Trigger ArmLowerButton = m_operatorController.axisGreaterThan(1, .25);
-        // final Trigger ShooterArmRaiseButton = m_operatorController.axisLessThan(5,
-        // -.25);
-        // final Trigger ShooterArmLowerButton = m_operatorController.axisGreaterThan(1,
-        // .25);
-
-        final Trigger WinchForwardButton = m_operatorController.axisLessThan(5, -.25);
-        final Trigger WinchBackButton = m_operatorController.axisGreaterThan(1, .25);
-
-        final Trigger ClimberWinchButton = m_operatorController.povDown();
-
-        intake.whileTrue(new InstantCommand(m_intake::run))
-                .whileFalse(new InstantCommand(m_intake::stop));
-
-        moveToTravel.whileTrue(new MoveToTravel(m_arm));
-
-        moveToSubwoofer.whileTrue(new MoveToSubwooferLow(m_arm));
-
-        moveToAmp.whileTrue(new MoveToAmp(m_arm));
-
-        moveToPodium.whileTrue(new MoveToPodium(m_arm));
-
-        // TODO: Test the "ENUM approach" by pressing X here.
-        moveToPodium.whileTrue(new MoveToPosition(m_arm, ArmConstants.Position.PODIUM));
-
-        moveToTrapApproach.whileTrue(new MoveToTrapApproach(m_arm));
-
-        moveToTrapScore.whileTrue(new MoveToTrapScore(m_arm));
-
-        moveToTrapClimb.whileTrue(new MoveToTrapClimb(m_arm));
+        // The driver's controller
+        CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
+        // The operator's controller
+        CommandXboxController m_operatorController = new CommandXboxController(OIConstants.kOperatorControllerPort);
 
         /**
-         * Alternate positions.
-         * For these, you need to hold down the Left Bumper too.
-         **/
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                // Configure the button bindings
+                configureButtonBindings();
+                addToDashboard();
 
-        // Move to HIGH Podium
-        moveToPodium.and(alternatePosition).whileTrue(
-                new InstantCommand(() -> m_arm.setArmPositionDegrees(ArmConstants.kHighPodiumArm)));
+                // Configure default commands
+                m_drive.setDefaultCommand(
+                                // The left stick controls translation of the robot.
+                                // Turning is controlled by the X axis of the right stick.
+                                new RunCommand(() -> m_drive.drive(
+                                                -MathUtil.applyDeadband(m_driverController.getLeftY(),
+                                                                OIConstants.kDriveDeadband),
+                                                -MathUtil.applyDeadband(m_driverController.getLeftX(),
+                                                                OIConstants.kDriveDeadband),
+                                                -MathUtil.applyDeadband(m_driverController.getRightX(),
+                                                                OIConstants.kDriveDeadband),
+                                                true,
+                                                true),
+                                                m_drive));
+        }
 
-        // Move to BACK Podium
-        moveToAmp.and(alternatePosition).whileTrue(
-                new InstantCommand(() -> m_arm.setArmPositionDegrees(ArmConstants.kBackPodiumArm)));
+        private void addToDashboard() {
+                SmartDashboard.putData("Move to Podium", new MoveToPosition(m_arm, ArmConstants.Position.PODIUM));
 
-        // Move to HIGH Subwoofer
-        moveToSubwoofer.and(alternatePosition).whileTrue(
-                new InstantCommand(() -> m_arm.setArmPositionDegrees(ArmConstants.kHighSubwooferArm)));
+        }
 
-        // Manual Arm and Shooter Movements
+        /**
+         * Use this method to define your button->command mappings. Buttons can be
+         */
+        private void configureButtonBindings() {
+                // DRIVER controlled buttons
+                final Trigger shoot = m_driverController.a();
 
-        ArmRaiseButton.whileTrue(new InstantCommand(m_arm::armRaise, m_arm))
-                .onFalse(new InstantCommand(
-                        () -> m_arm.keepArmPosition(
-                                m_arm.getCurrentArmPosition())));
+                // OPERATOR controlled buttons
+                final Trigger intake = m_operatorController.leftTrigger();
+                final Trigger alternatePosition = m_operatorController.leftBumper();
 
-        ArmLowerButton.whileTrue(new InstantCommand(m_arm::armLower, m_arm))
-                .onFalse(new InstantCommand(
-                        () -> m_arm.keepArmPosition(
-                                m_arm.getCurrentArmPosition())));
+                final Trigger moveToTravel = m_operatorController.y();
+                final Trigger moveToSubwoofer = m_operatorController.b();
+                final Trigger moveToAmp = m_operatorController.a();
+                final Trigger moveToPodium = m_operatorController.x();
+                final Trigger moveToTrapApproach = m_operatorController.povUp();
+                final Trigger moveToTrapScore = m_operatorController.povRight();
+                final Trigger moveToTrapClimb = m_operatorController.povDown();
 
-        // ShooterArmRaiseButton.whileTrue(new InstantCommand(m_arm::shooterArmRaise,
-        // m_arm))
-        // .onFalse(new InstantCommand(
-        // () -> m_arm.keepArmPosition(
-        // m_arm.getCurrentArmPosition())));
+                // manual arm and shooter movement - arm left joystick, shooter right joystick
+                final Trigger ArmRaiseButton = m_operatorController.axisLessThan(1, -.25);
+                final Trigger ArmLowerButton = m_operatorController.axisGreaterThan(1, .25);
+                // final Trigger ShooterArmRaiseButton = m_operatorController.axisLessThan(5,
+                // -.25);
+                // final Trigger ShooterArmLowerButton = m_operatorController.axisGreaterThan(5,
+                // .25);
 
-        // ShooterArmLowerButton.whileTrue(new InstantCommand(m_arm::shooterArmLower,
-        // m_arm))
-        // .onFalse(new InstantCommand(
-        // () -> m_arm.keepArmPosition(
-        // m_arm.getCurrentArmPosition())));
+                final Trigger WinchForwardButton = m_operatorController.axisLessThan(5, -.25);
+                final Trigger WinchBackButton = m_operatorController.axisGreaterThan(5, .25);
 
-        WinchForwardButton.whileTrue(
-                new InstantCommand(m_climb::forward))
-                .onFalse(new InstantCommand(m_climb::stop));
+                final Trigger ClimberWinchButton = m_operatorController.povDown();
 
-        WinchBackButton.whileTrue(
-                new InstantCommand(m_climb::back))
-                .onFalse(new InstantCommand(m_climb::stop));
+                intake.whileTrue(new InstantCommand(m_intake::forward))
+                                .whileFalse(new InstantCommand(m_intake::stop));
 
-    }
+                moveToTravel.whileTrue(new MoveToTravel(m_arm));
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // Create config for trajectory
-        TrajectoryConfig config = new TrajectoryConfig(
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                // Add kinematics to ensure max speed is actually obeyed
-                .setKinematics(DriveConstants.kDriveKinematics);
+                moveToSubwoofer.whileTrue(new MoveToSubwooferLow(m_arm));
 
-        // An example trajectory to follow. All units in meters.
-        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, new Rotation2d(0)),
-                config);
+                moveToAmp.whileTrue(new MoveToAmp(m_arm));
 
-        var thetaController = new ProfiledPIDController(
-                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+                moveToPodium.whileTrue(new MoveToPodium(m_arm));
 
-        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-                exampleTrajectory,
-                m_drive::getPose, // Functional interface to feed supplier
-                DriveConstants.kDriveKinematics,
+                // TODO: Test the "ENUM approach" by pressing X here.
+                moveToPodium.whileTrue(new MoveToPosition(m_arm, ArmConstants.Position.PODIUM));
 
-                // Position controllers
-                new PIDController(AutoConstants.kPXController, 0, 0),
-                new PIDController(AutoConstants.kPYController, 0, 0),
-                thetaController,
-                m_drive::setModuleStates,
-                m_drive);
+                moveToTrapApproach.whileTrue(new MoveToTrapApproach(m_arm));
 
-        // Reset odometry to the starting pose of the trajectory.
-        m_drive.resetOdometry(exampleTrajectory.getInitialPose());
+                moveToTrapScore.whileTrue(new MoveToTrapScore(m_arm));
 
-        // Run path following command, then stop at the end.
-        return swerveControllerCommand.andThen(() -> m_drive.drive(0, 0, 0, false, false));
-    }
+                moveToTrapClimb.whileTrue(new MoveToTrapClimb(m_arm));
+
+                /**
+                 * Alternate positions.
+                 * For these, you need to hold down the Left Bumper too.
+                 **/
+
+                // Move to HIGH Podium
+                moveToPodium.and(alternatePosition).whileTrue(
+                                new InstantCommand(() -> m_arm.setArmPositionDegrees(ArmConstants.kHighPodiumArm)));
+
+                // Move to BACK Podium
+                moveToAmp.and(alternatePosition).whileTrue(
+                                new InstantCommand(() -> m_arm.setArmPositionDegrees(ArmConstants.kBackPodiumArm)));
+
+                // Move to HIGH Subwoofer
+                moveToSubwoofer.and(alternatePosition).whileTrue(
+                                new InstantCommand(() -> m_arm.setArmPositionDegrees(ArmConstants.kHighSubwooferArm)));
+
+                // Manual Arm and Shooter Movements
+
+                ArmRaiseButton.whileTrue(new InstantCommand(m_arm::armRaise, m_arm))
+                                .onFalse(new InstantCommand(
+                                                () -> m_arm.keepArmPosition(
+                                                                m_arm.getCurrentArmPosition())));
+
+                ArmLowerButton.whileTrue(new InstantCommand(m_arm::armLower, m_arm))
+                                .onFalse(new InstantCommand(
+                                                () -> m_arm.keepArmPosition(
+                                                                m_arm.getCurrentArmPosition())));
+
+                // ShooterArmRaiseButton.whileTrue(new InstantCommand(m_arm::shooterArmRaise,
+                // m_arm))
+                // .onFalse(new InstantCommand(
+                // () -> m_arm.keepArmPosition(
+                // m_arm.getCurrentArmPosition())));
+
+                // ShooterArmLowerButton.whileTrue(new InstantCommand(m_arm::shooterArmLower,
+                // m_arm))
+                // .onFalse(new InstantCommand(
+                // () -> m_arm.keepArmPosition(
+                // m_arm.getCurrentArmPosition())));
+
+                WinchForwardButton.whileTrue(
+                                new InstantCommand(m_climb::forward))
+                                .onFalse(new InstantCommand(m_climb::stop));
+
+                WinchBackButton.whileTrue(
+                                new InstantCommand(m_climb::back))
+                                .onFalse(new InstantCommand(m_climb::stop));
+
+        }
+
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                // Create config for trajectory
+                TrajectoryConfig config = new TrajectoryConfig(
+                                AutoConstants.kMaxSpeedMetersPerSecond,
+                                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                                // Add kinematics to ensure max speed is actually obeyed
+                                .setKinematics(DriveConstants.kDriveKinematics);
+
+                // An example trajectory to follow. All units in meters.
+                Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+                                // Start at the origin facing the +X direction
+                                new Pose2d(0, 0, new Rotation2d(0)),
+                                // Pass through these two interior waypoints, making an 's' curve path
+                                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                                // End 3 meters straight ahead of where we started, facing forward
+                                new Pose2d(3, 0, new Rotation2d(0)),
+                                config);
+
+                var thetaController = new ProfiledPIDController(
+                                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+                thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+                SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+                                exampleTrajectory,
+                                m_drive::getPose, // Functional interface to feed supplier
+                                DriveConstants.kDriveKinematics,
+
+                                // Position controllers
+                                new PIDController(AutoConstants.kPXController, 0, 0),
+                                new PIDController(AutoConstants.kPYController, 0, 0),
+                                thetaController,
+                                m_drive::setModuleStates,
+                                m_drive);
+
+                // Reset odometry to the starting pose of the trajectory.
+                m_drive.resetOdometry(exampleTrajectory.getInitialPose());
+
+                // Run path following command, then stop at the end.
+                return swerveControllerCommand.andThen(() -> m_drive.drive(0, 0, 0, false, false));
+        }
 }
