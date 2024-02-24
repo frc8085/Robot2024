94476f8b558a6a0b784c03e7a459acdefae3f90c
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2024-02-24 17:48:45 -0500

    Fine tune Trap Shot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -84,7 +84,7 @@ public final class Constants {
                 public static double kFeederI = 0.0001;
                 public static double kFeederD = 0;
 
-                public static double kFeederSetPoint = 3000;
+                public static double kFeederSetPoint = 4000;
 
         }
 
@@ -255,7 +255,7 @@ public final class Constants {
                                         false,
                                         false),
                         TRAP_SCORE("Trap Score",
-                                        310,
+                                        315,
                                         90,
                                         true,
                                         true,
