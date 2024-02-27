47e3b7bc84b64c086d3b46acfed23302a09bb813
lseluga <loriann.higashi@yale.edu>
2024-02-27 13:20:07 -0500

    added new pickup for auto so we can test adjusting feeder wheel speed and/or intake position

---
+++ src/main/java/frc/robot/commands/PickUpNoteAuto.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants.ArmConstants.Position;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.Blinkin;
+import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class PickUpNoteAuto extends SequentialCommandGroup {
+        public PickUpNoteAuto(
+                        IntakeSubsystem m_intake,
+                        FeederSubsystem m_feeder,
+                        ArmSubsystem m_arm,
+                        ShooterSubsystem m_shooter,
+                        Blinkin m_blinkin) {
+                addCommands(
+                                new ConditionalCommand(new InstantCommand(),
+                                                new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin,
+                                                                Position.HOME),
+                                                m_arm::atHomePosition),
+                                new WaitUntilCommand(m_arm::atHomePosition),
+                                new ParallelCommandGroup(
+                                                new InstantCommand(m_blinkin::intakeOn),
+                                                new InstantCommand(m_intake::run),
+                                                new InstantCommand(m_feeder::runAuto)),
+                                new WaitUntilCommand(m_feeder::isNoteDetected),
+                                new PickUpNoteCompleted(m_intake, m_feeder, m_blinkin));
+        }
+}
