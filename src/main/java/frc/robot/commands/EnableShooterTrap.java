2db568a7169b82a6535fb6b289ed8f994ed378b1
lseluga <loriann.higashi@yale.edu>
2024-02-27 15:38:32 -0500

    code cleanup, renamed command for clarity

--- src/main/java/frc/robot/commands/EnableShooterTrap.java
+++ src/main/java/frc/robot/commands/EnableShooterTrap.java
@@ -6,12 +6,12 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class EnableShooterAuto extends SequentialCommandGroup {
-        public EnableShooterAuto(
+public class EnableShooterTrap extends SequentialCommandGroup {
+        public EnableShooterTrap(
                         FeederSubsystem m_feeder,
                         ShooterSubsystem m_shooter) {
                 addCommands(
                                 new NoteCorrection(m_feeder),
-                                new InstantCommand(m_shooter::run));
+                                new InstantCommand(m_shooter::runTrap));
         }
 }
