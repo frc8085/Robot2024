41d0d8f2347ee4ac3e25530a38aaefc6ab3b7b2c
lseluga <loriann.higashi@yale.edu>
2024-02-27 13:20:36 -0500

    cleanup file to use note correction sequence

--- src/main/java/frc/robot/commands/PickUpNoteCompleted.java
+++ src/main/java/frc/robot/commands/PickUpNoteCompleted.java
@@ -2,8 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.FeederConstants;
@@ -17,15 +16,16 @@ public class PickUpNoteCompleted extends SequentialCommandGroup {
                         FeederSubsystem m_feeder,
                         Blinkin m_blinkin) {
                 addCommands(
+                                // Set boolean that the Note has been picked up
                                 new InstantCommand(m_feeder::notePickedUp),
                                 new ParallelCommandGroup(
                                                 new InstantCommand(m_intake::stop),
                                                 new InstantCommand(m_feeder::stop)),
                                 new WaitCommand(FeederConstants.kLoadWaitTime),
-                                new InstantCommand(m_feeder::runBackwards),
-                                new WaitUntilCommand(
-                                                () -> m_feeder.isNoteNotDetected()),
-                                new InstantCommand(m_feeder::stop),
+                                // Check if Note is touching shooter wheels and run backwards if needed
+                                new ConditionalCommand(new NoteCorrection(m_feeder), new InstantCommand(),
+                                                m_feeder::needNoteCorrection),
+                                // Change the LEDs to show note is in robot
                                 new InstantCommand(m_blinkin::withNote));
         }
 }
