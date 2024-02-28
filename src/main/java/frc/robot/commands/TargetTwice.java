284b8b2b11595dd4511efa136f25fbbcef1e768b
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2024-02-28 15:23:13 -0500

    Added auto y targeting to target twice but as a comment as im not sure if this is where it goes.

--- src/main/java/frc/robot/commands/TargetTwice.java
+++ src/main/java/frc/robot/commands/TargetTwice.java
@@ -6,20 +6,32 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.ArmSubsystem;
 
 public class TargetTwice extends SequentialCommandGroup {
-        public TargetTwice(
-                        LimelightSubsystem m_limelight,
-                        DriveSubsystem m_drive) {
-                addCommands(
-                                new ConditionalCommand(
-                                                new AutoTarget(m_limelight, m_drive, true),
-                                                new InstantCommand(),
-                                                m_limelight::hasTarget),
-                                new WaitCommand(0.1),
-                                new ConditionalCommand(
-                                                new AutoTarget(m_limelight, m_drive, true),
-                                                new InstantCommand(),
-                                                m_limelight::hasTarget));
-        }
+    public TargetTwice(
+            LimelightSubsystem m_limelight,
+            DriveSubsystem m_drive,
+            ArmSubsystem m_arm) {
+        addCommands(
+                new ConditionalCommand(
+                        new AutoTarget(m_limelight, m_drive, true),
+                        new InstantCommand(),
+                        m_limelight::hasTarget),
+                new WaitCommand(0.1),
+                new ConditionalCommand(
+                        new AutoTarget(m_limelight, m_drive, true),
+                        new InstantCommand(),
+                        m_limelight::hasTarget),
+                new WaitCommand(0.1),
+                new ConditionalCommand(
+                        new AutoYAim(m_limelight, m_arm, true),
+                        new InstantCommand(),
+                        m_limelight::hasTarget),
+                new WaitCommand(0.1),
+                new ConditionalCommand(
+                        new AutoYAim(m_limelight, m_arm, true),
+                        new InstantCommand(),
+                        m_limelight::hasTarget));
+    }
 }
