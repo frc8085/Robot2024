fd86fe206e166728fdff7cb62121ca9956ed7981
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2024-02-28 15:13:06 -0500

    Revert "added y auto targeting (needs testing)"
    This reverts commit 2a7eb081bf427d413aa52bcc197233d106221107.

--- src/main/java/frc/robot/commands/AutoAimAndShoot.java
+++ src/main/java/frc/robot/commands/AutoAimAndShoot.java
@@ -4,18 +4,16 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ArmConstants.Position;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutoAimAndShoot extends SequentialCommandGroup {
     public AutoAimAndShoot(
             ArmSubsystem m_arm,
             ShooterSubsystem m_shooter,
-            LimelightSubsystem m_limelight,
             Position position) {
         addCommands(
                 new InstantCommand(() -> System.out.println("**START Move to " + position.label + " Position**")),
-                new InstantCommand(() -> m_arm.setShooterPivotPosition(m_limelight.getYfromRobotPerspective())),
+                new InstantCommand(() -> m_arm.moveToPosition(position)),
                 new InstantCommand(m_shooter::run));
     }
 
