cb8a224f7fdd9276f4380326365bb81c9ee4f5d0
lseluga <loriann.higashi@yale.edu>
2024-02-29 11:35:07 -0500

    add target twice for shooter pivot

---
+++ src/main/java/frc/robot/commands/TargetSPTwice.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
+
+public class TargetSPTwice extends SequentialCommandGroup {
+        public TargetSPTwice(
+                        LimelightSubsystem m_limelight,
+                        ArmSubsystem m_arm) {
+                addCommands(
+                                new ConditionalCommand(
+                                                new AutoTargetSP(m_limelight, m_arm),
+                                                new InstantCommand(),
+                                                m_limelight::hasTarget),
+                                new WaitCommand(0.1),
+                                new ConditionalCommand(
+                                                new AutoTargetSP(m_limelight, m_arm),
+                                                new InstantCommand(),
+                                                m_limelight::hasTarget));
+        }
+}
