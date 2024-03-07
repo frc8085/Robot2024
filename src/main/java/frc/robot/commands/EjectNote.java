dc9558dfc96bdc568c799d0193020e184c87f168
lseluga <loriann.higashi@yale.edu>
2024-03-07 17:29:49 -0500

    remove unused imports

--- src/main/java/frc/robot/commands/EjectNote.java
+++ src/main/java/frc/robot/commands/EjectNote.java
@@ -1,13 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.Constants.ArmConstants.Position;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.Blinkin;
--- src/main/java/frc/robot/commands/ShootNew.java
+++ src/main/java/frc/robot/commands/ShootNew.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
-import java.util.concurrent.locks.Condition;
-
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.ArmConstants.Position;
@@ -15,27 +13,27 @@ import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShootNew extends SequentialCommandGroup {
-    public ShootNew(
-            FeederSubsystem m_feeder,
-            ArmSubsystem m_arm,
-            ShooterSubsystem m_shooter,
-            Blinkin m_blinkin,
-            Position position) {
-        addCommands(
-                new ConditionalCommand(
-                        new WaitUntilCommand(m_shooter::readyToShootSW),
-                        new WaitUntilCommand(m_shooter::readyToShootSW),
-                        m_arm::atPodiumPosition),
-                new InstantCommand(m_feeder::run),
-                // watch for isNoteDetected to be true and then turn off the feeder after X
-                // seconds
-                new WaitUntilCommand(m_feeder::isNoteDetected),
-                new WaitUntilCommand(m_feeder::isNoteNotDetected),
-                new WaitCommand(.2),
-                new InstantCommand(m_feeder::stop),
-                new InstantCommand(m_shooter::stop),
-                new InstantCommand(m_feeder::noteShot),
-                new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin, position),
-                new InstantCommand(m_blinkin::driving));
-    }
+        public ShootNew(
+                        FeederSubsystem m_feeder,
+                        ArmSubsystem m_arm,
+                        ShooterSubsystem m_shooter,
+                        Blinkin m_blinkin,
+                        Position position) {
+                addCommands(
+                                new ConditionalCommand(
+                                                new WaitUntilCommand(m_shooter::readyToShootSW),
+                                                new WaitUntilCommand(m_shooter::readyToShootSW),
+                                                m_arm::atPodiumPosition),
+                                new InstantCommand(m_feeder::run),
+                                // watch for isNoteDetected to be true and then turn off the feeder after X
+                                // seconds
+                                new WaitUntilCommand(m_feeder::isNoteDetected),
+                                new WaitUntilCommand(m_feeder::isNoteNotDetected),
+                                new WaitCommand(.2),
+                                new InstantCommand(m_feeder::stop),
+                                new InstantCommand(m_shooter::stop),
+                                new InstantCommand(m_feeder::noteShot),
+                                new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin, position),
+                                new InstantCommand(m_blinkin::driving));
+        }
 }
