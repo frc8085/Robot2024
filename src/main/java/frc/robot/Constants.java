3798c1bb5b1173743b868d07c247249850b361a9
lseluga <loriann.higashi@yale.edu>
2024-03-27 18:50:12 -0400

    sped up shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -111,7 +111,7 @@ public final class Constants {
                 public static double kShooterEncoder1VelocityFactor = (2 * Math.PI) / 60.0;
                 public static double kShooterEncoder1PositionFactor = (2 * Math.PI);
 
-                public static double kShooter1SetPoint = 1200;
+                public static double kShooter1SetPoint = 2000;
                 public static double kShooter2SetPoint = kShooter1SetPoint * 1;
                 public static double kShooter1PodiumToleranceRPMPercent = .1;
                 public static double kShooter2PodiumToleranceRPMPercent = .1;
@@ -447,7 +447,7 @@ public final class Constants {
         public static final class DriveConstants {
                 // Driving Parameters - Note that these are not the maximum capable speeds of
                 // the robot, rather the allowed maximum speeds
-                public static final double kMaxSpeedAdjustment = .25;
+                public static final double kMaxSpeedAdjustment = .1;
                 public static final double kMaxSpeedMetersPerSecond = 5.75 * kMaxSpeedAdjustment;
 
                 // if you want to slow down the rotation speed, change the adjustment factor
