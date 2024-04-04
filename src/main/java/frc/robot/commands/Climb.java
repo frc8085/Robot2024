9e19989161e5e91baef2db3c57e2ec66cfc162d1
adrianacorona <adriana.corona@gmail.com>
2024-04-03 21:34:35 -0400

    Winch raise override; rename forward/back to raise/lower

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -11,32 +11,32 @@ import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class Climb extends SequentialCommandGroup {
-    public Climb(
-            FeederSubsystem m_feeder,
-            ArmSubsystem m_arm,
-            ShooterSubsystem m_shooter,
-            ClimberSubsystem m_climber,
-            Blinkin m_blinkin) {
-        addCommands(
-                // Set Position to Trap Approach
-                new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin, Position.TRAP_APPROACH),
+  public Climb(
+      FeederSubsystem m_feeder,
+      ArmSubsystem m_arm,
+      ShooterSubsystem m_shooter,
+      ClimberSubsystem m_climber,
+      Blinkin m_blinkin) {
+    addCommands(
+        // Set Position to Trap Approach
+        new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin, Position.TRAP_APPROACH),
 
-                // Move robot centered on chain against wall using limelight
+        // Move robot centered on chain against wall using limelight
 
-                // Start Climbing until it can move to the next position
-                new InstantCommand(m_climber::forward),
+        // Start Climbing until it can move to the next position
+        new InstantCommand(m_climber::raise),
 
-                // when it gets to the correct height, change to trap score position and turn on
-                // shooter, but keep climbing up
-                new WaitUntilCommand(m_climber::atMoveToTrapScore),
-                new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin, Position.TRAP_SCORE),
-                new InstantCommand(m_shooter::runTrap),
+        // when it gets to the correct height, change to trap score position and turn on
+        // shooter, but keep climbing up
+        new WaitUntilCommand(m_climber::atMoveToTrapScore),
+        new MoveToPosition(m_arm, m_shooter, m_feeder, m_blinkin, Position.TRAP_SCORE),
+        new InstantCommand(m_shooter::runTrap),
 
-                // when it gets to the trap height, stop climbing (ideally we'd hold with a PID)
-                new WaitUntilCommand(m_climber::atTrapScore),
-                new InstantCommand(m_climber::stop),
+        // when it gets to the trap height, stop climbing (ideally we'd hold with a PID)
+        new WaitUntilCommand(m_climber::atTrapScore),
+        new InstantCommand(m_climber::stop),
 
-                // shoot note
-                new ShootTrap(m_feeder, m_arm, m_shooter, m_blinkin));
-    }
+        // shoot note
+        new ShootTrap(m_feeder, m_arm, m_shooter, m_blinkin));
+  }
 }
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -15,157 +15,164 @@ import frc.robot.Constants.MotorDefaultsConstants;
 import frc.robot.Constants.TuningModeConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
-    private boolean TUNING_MODE = TuningModeConstants.kClimberTuning;
-    private boolean PRACTICE_MODE = TuningModeConstants.kPracticeMode;
+  private boolean TUNING_MODE = TuningModeConstants.kClimberTuning;
+  private boolean PRACTICE_MODE = TuningModeConstants.kPracticeMode;
 
-    private final CANSparkFlex m_winchMotor = new CANSparkFlex(
-            CanIdConstants.kWinchCanId, MotorDefaultsConstants.NeoVortexMotorType);
+  private final CANSparkFlex m_winchMotor = new CANSparkFlex(
+      CanIdConstants.kWinchCanId, MotorDefaultsConstants.NeoVortexMotorType);
 
-    // Encoder
-    // private RelativeEncoder m_winchEncoder;
-    private SparkAbsoluteEncoder m_winchEncoder;
+  // Encoder
+  // private RelativeEncoder m_winchEncoder;
+  private SparkAbsoluteEncoder m_winchEncoder;
 
-    // PID for Winch
-    private SparkPIDController m_winchPIDController = m_winchMotor.getPIDController();
+  // PID for Winch
+  private SparkPIDController m_winchPIDController = m_winchMotor.getPIDController();
 
-    // PID Constants for tuning
-    double kWinchP = ClimberConstants.kWinchP;
-    double kWinchI = ClimberConstants.kWinchI;
-    double kWinchD = ClimberConstants.kWinchD;
-    double kWinchFF = ClimberConstants.kWinchFF;
-    double kWinchMaxOutput = ClimberConstants.kWinchMaxOutput;
-    double kWinchMinOutput = ClimberConstants.kWinchMinOutput;
-    double kWinchMaxAccel = ClimberConstants.kWinchMaxAccel;
-    double kWinchMaxVelo = ClimberConstants.kWinchMaxVelo;
+  // PID Constants for tuning
+  double kWinchP = ClimberConstants.kWinchP;
+  double kWinchI = ClimberConstants.kWinchI;
+  double kWinchD = ClimberConstants.kWinchD;
+  double kWinchFF = ClimberConstants.kWinchFF;
+  double kWinchMaxOutput = ClimberConstants.kWinchMaxOutput;
+  double kWinchMinOutput = ClimberConstants.kWinchMinOutput;
+  double kWinchMaxAccel = ClimberConstants.kWinchMaxAccel;
+  double kWinchMaxVelo = ClimberConstants.kWinchMaxVelo;
 
-    // limit switches
-    private SparkLimitSwitch m_winchLeftLowerLimit;
-    private SparkLimitSwitch m_winchRightLowerLimit;
-    private SparkLimitSwitch m_winchLeftRaiseLimit;
-    private SparkLimitSwitch m_winchRightRaiseLimit;
+  // limit switches
+  private SparkLimitSwitch m_winchLeftLowerLimit;
+  private SparkLimitSwitch m_winchRightLowerLimit;
+  private SparkLimitSwitch m_winchLeftRaiseLimit;
+  private SparkLimitSwitch m_winchRightRaiseLimit;
 
-    public boolean trapPressed = false;
+  public boolean trapPressed = false;
+  private boolean ignoringWinchRaseLimit = false;
 
-    public boolean WinchLowerLimitHit() {
-        return isWinchLowerLimitHit();
-    }
+  public boolean WinchLowerLimitHit() {
+    return isWinchLowerLimitHit();
+  }
 
-    public boolean WinchRaiseLimitHit() {
-        return isWinchRaiseLimitHit();
-    }
+  public boolean WinchRaiseLimitHit() {
+    return isWinchRaiseLimitHit();
+  }
 
-    public boolean isWinchLowerLimitHit() {
-        return m_winchLeftLowerLimit.isPressed() || m_winchRightLowerLimit.isPressed();
-    }
+  public boolean isWinchLowerLimitHit() {
+    return m_winchLeftLowerLimit.isPressed() || m_winchRightLowerLimit.isPressed();
+  }
 
-    public boolean isWinchRaiseLimitHit() {
-        return m_winchLeftRaiseLimit.isPressed() || m_winchRightRaiseLimit.isPressed();
-    }
+  public boolean isWinchRaiseLimitHit() {
+    return m_winchLeftRaiseLimit.isPressed() || m_winchRightRaiseLimit.isPressed();
+  }
 
-    // Creates new Climber Subsystem
-    public ClimberSubsystem() {
-        m_winchMotor.restoreFactoryDefaults();
-        m_winchMotor.setIdleMode(ClimberConstants.kWinchMotorIdleMode);
-        m_winchMotor.setSmartCurrentLimit(MotorDefaultsConstants.NeoVortexCurrentLimit);
+  // Creates new Climber Subsystem
+  public ClimberSubsystem() {
+    m_winchMotor.restoreFactoryDefaults();
+    m_winchMotor.setIdleMode(ClimberConstants.kWinchMotorIdleMode);
+    m_winchMotor.setSmartCurrentLimit(MotorDefaultsConstants.NeoVortexCurrentLimit);
 
-        // Setup encoders and PID controllers for the arm and shooter arms.
+    // Setup encoders and PID controllers for the arm and shooter arms.
 
-        // alternate encoder
-        // m_winchEncoder = m_winchMotor.getEncoder(RelativeEncoder.class, 8192);
-        // absolute encoder
-        m_winchEncoder = m_winchMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
-        m_winchPIDController.setFeedbackDevice(m_winchEncoder);
+    // alternate encoder
+    // m_winchEncoder = m_winchMotor.getEncoder(RelativeEncoder.class, 8192);
+    // absolute encoder
+    m_winchEncoder = m_winchMotor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+    m_winchPIDController.setFeedbackDevice(m_winchEncoder);
 
-        // Set if encoder is inverted
-        m_winchEncoder.setInverted(false);
+    // Set if encoder is inverted
+    m_winchEncoder.setInverted(false);
 
-        // Set the PID gains for the turning motor.
-        m_winchPIDController.setFeedbackDevice(m_winchEncoder);
-        m_winchPIDController.setP(ClimberConstants.kWinchP);
-        m_winchPIDController.setI(ClimberConstants.kWinchI);
-        m_winchPIDController.setD(ClimberConstants.kWinchD);
-        m_winchPIDController.setFF(ClimberConstants.kWinchFF);
-        m_winchPIDController.setOutputRange(ClimberConstants.kWinchMinOutput,
-                ClimberConstants.kWinchMaxOutput);
-        m_winchPIDController.setSmartMotionMaxAccel(ClimberConstants.kWinchMaxAccel, 0);
-        m_winchPIDController.setSmartMotionMaxVelocity(ClimberConstants.kWinchMaxVelo, 0);
+    // Set the PID gains for the turning motor.
+    m_winchPIDController.setFeedbackDevice(m_winchEncoder);
+    m_winchPIDController.setP(ClimberConstants.kWinchP);
+    m_winchPIDController.setI(ClimberConstants.kWinchI);
+    m_winchPIDController.setD(ClimberConstants.kWinchD);
+    m_winchPIDController.setFF(ClimberConstants.kWinchFF);
+    m_winchPIDController.setOutputRange(ClimberConstants.kWinchMinOutput,
+        ClimberConstants.kWinchMaxOutput);
+    m_winchPIDController.setSmartMotionMaxAccel(ClimberConstants.kWinchMaxAccel, 0);
+    m_winchPIDController.setSmartMotionMaxVelocity(ClimberConstants.kWinchMaxVelo, 0);
 
-        // Limit switches
-        m_winchLeftLowerLimit = m_winchMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
-        m_winchRightLowerLimit = m_winchMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
-        m_winchLeftRaiseLimit = m_winchMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
-        m_winchRightRaiseLimit = m_winchMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    // Limit switches
+    m_winchLeftLowerLimit = m_winchMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    m_winchRightLowerLimit = m_winchMotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    m_winchLeftRaiseLimit = m_winchMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+    m_winchRightRaiseLimit = m_winchMotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
 
-        m_winchMotor.burnFlash();
+    m_winchMotor.burnFlash();
 
-        if (TUNING_MODE) {
-
-        }
-
-    }
-
-    public void log() {
-        if (LoggingConstants.kLogging) {
-        }
-    }
-
-    public void practiceDashboard() {
-        SmartDashboard.putNumber("Winch Position", getWinchPosition());
-        SmartDashboard.putNumber("Winch Current", m_winchMotor.getOutputCurrent());
-    }
-
-    // Returns the winch
-    public double getWinchPosition() {
-        return m_winchEncoder.getPosition();
-    }
-
-    public void setWinchPosition(double winchPosition) {
-        m_winchPIDController.setReference(winchPosition, ControlType.kPosition);
-        if (TUNING_MODE) {
-            SmartDashboard.putNumber("Desired Winch Position", winchPosition);
-        }
-    }
-
-    // Open Loop Control
-    public void forward() {
-        m_winchMotor.set(ClimberConstants.kRaiseSpeed);
-        System.out.println("Winch Encoder " + getWinchPosition());
-    }
-
-    public void back() {
-        m_winchMotor.set(-ClimberConstants.kLowerSpeed);
-    }
-
-    public void stop() {
-        m_winchMotor.set(0);
-    }
-
-    public boolean atMoveToTrapScore() {
-        return (getWinchPosition() > (ClimberConstants.climbMoveToTrapScore));
-    }
-
-    public boolean atTrapScore() {
-        return (getWinchPosition() > (ClimberConstants.climbAtTrapScore));
-    }
-
-    public void trapPressedNow() {
-        trapPressed = true;
-    }
-
-    public boolean isTrapPressed() {
-        return trapPressed;
-    }
-
-    public void periodic() {
-        // This method will be called once per scheduler run
-
-        log();
-        if (PRACTICE_MODE) {
-            practiceDashboard();
-        }
-        if (TUNING_MODE) {
-        }
+    if (TUNING_MODE) {
 
     }
+
+  }
+
+  public void log() {
+    if (LoggingConstants.kLogging) {
+    }
+  }
+
+  public void practiceDashboard() {
+    SmartDashboard.putNumber("Winch Position", getWinchPosition());
+    SmartDashboard.putNumber("Winch Current", m_winchMotor.getOutputCurrent());
+  }
+
+  // Returns the winch
+  public double getWinchPosition() {
+    return m_winchEncoder.getPosition();
+  }
+
+  public void setWinchPosition(double winchPosition) {
+    m_winchPIDController.setReference(winchPosition, ControlType.kPosition);
+    if (TUNING_MODE) {
+      SmartDashboard.putNumber("Desired Winch Position", winchPosition);
+    }
+  }
+
+  // Open Loop Control
+  public void raise() {
+    if (!isWinchRaiseLimitHit() || ignoringWinchRaseLimit) {
+      m_winchMotor.set(ClimberConstants.kRaiseSpeed);
+    }
+    System.out.println("Winch Encoder " + getWinchPosition());
+  }
+
+  public void ignoreWinchRaseLimit() {
+    ignoringWinchRaseLimit = true;
+  }
+
+  public void lower() {
+    m_winchMotor.set(-ClimberConstants.kLowerSpeed);
+  }
+
+  public void stop() {
+    m_winchMotor.set(0);
+  }
+
+  public boolean atMoveToTrapScore() {
+    return (getWinchPosition() > (ClimberConstants.climbMoveToTrapScore));
+  }
+
+  public boolean atTrapScore() {
+    return (getWinchPosition() > (ClimberConstants.climbAtTrapScore));
+  }
+
+  public void trapPressedNow() {
+    trapPressed = true;
+  }
+
+  public boolean isTrapPressed() {
+    return trapPressed;
+  }
+
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+    log();
+    if (PRACTICE_MODE) {
+      practiceDashboard();
+    }
+    if (TUNING_MODE) {
+    }
+
+  }
 
 }
