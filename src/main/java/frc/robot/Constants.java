c0eef330fc842c2ba568e7330485caa804896bd8
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2024-09-18 19:57:13 -0400

    Began coding of wheel speed calculations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,27 +5,36 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 0;
+    }
 
-  public static class DriveConstants {
-      public static final int kMaxspeed = 10;
-      public static final int kPowerToFeet = 10;
-  }
-  
-  public static class CanIDConstants {
-      public static final int kFrontLeftCanID = 1;
-      public static final int kFrontRightCanID = 2;
-      public static final int kBackLeftCanID = 3;
-      public static final int kBackRightCanID = 4;
-  }
+    public static class DriveConstants {
+        public static final int kMaxspeed = 10;
+        public static final int kPowerToFeet = 10;
+    }
+
+    public static class CanIDConstants {
+        public static final int kFrontLeftCanID = 1;
+        public static final int kFrontRightCanID = 2;
+        public static final int kBackLeftCanID = 3;
+        public static final int kBackRightCanID = 4;
+    }
+
+    public static class RobotDimension {
+        public static final int kLengthFromCenter = 1;
+        public static final int kWidthFromCenter = 1;
+    }
 }
